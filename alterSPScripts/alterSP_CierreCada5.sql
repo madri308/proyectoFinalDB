a1cbb265d3357807d8fad691f56baee20c355ca7
Esteban Torres <esteban13torres@gmail.com>
2020-08-12 16:48:13 -0600

    SpCierreCada5

--- alterSPScripts/alterSP_CierreCada5.sql
+++ alterSPScripts/alterSP_CierreCada5.sql
@@ -4,29 +4,33 @@ IF OBJECT_ID('[dbo].[SP_CierreCada5]') IS NOT NULL
 BEGIN 
     DROP PROC [dbo].[SP_CierreCada5]  
 END 
-SET ANSI_NULLS ON
+SET ANSI_NULLS ON	
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROC [dbo].[SP_CierreCada5]
+CREATE PROC [dbo].[SP_CierreCada5] @inDate date
 AS 
 	BEGIN
 		BEGIN TRY
-		SET NOCOUNT ON 
-		SET XACT_ABORT ON  
+			SET NOCOUNT ON 
+			SET XACT_ABORT ON  
 		
-		DECLARE @idMenor INT, @idMayor INT
-		DEClARE @TelefonosNoPersonales Table (id int, idCliente int, Numero int)
-		-- Primero selecionar los telefonos no personales
+			DECLARE @idMenor INT, @idMayor INT, @idCierre INT
+		
+			SELECT @idMenor = min([id]), @idMayor=max([id]) FROM [ContratoExterno] -- ID MAYOR Y MENOR PARA ITERAR LA TABLA
+		
+			Begin Tran
+				insert into [Cierre] (FechaCierre) -- Se crea un nuevo elemento de cierre
+				values (@inDate)
 
+				set @idCierre = SCOPE_IDENTITY();
 
-		insert into @TelefonosNoPersonales (id, idCliente, Numero)
-		select [id],[idCliente],[Numero] 
-		from Contrato inner join TipoTarifa on Contrato.TipoTarifa = TipoTarifa.id
-		where TipoTarifa.id in (7,8,9,10,11,12)
-
-		SELECT @idMenor = min([id]), @idMayor=max([id]) FROM @TelefonosNoPersonales--SACA ID MAYOR Y MENOR PARA ITERAR LA TABLA
-
+				WHILE @idMenor<=@idMayor -- Se itera cada contrato externo
+					BEGIN
+						insert into [ContratosPorCierre] (idCierreExterno, idContratoExterno, CantidadMinutos)
+						select top 1 @idCierre, [SaldoMin800],[idContrato] from [FacturaExterna] where [FacturaExterna].[idContrato] = @idMenor ORDER BY [id] DESC
+					END
+			Commit
 		END TRY
 		BEGIN CATCH
 			THROW 55501,'Error, no se han podido realizar el cierre.',1;
